e2d8cfa78240741a18f5671cb2e571b330ed0531
fengchuanyu <fengchuanyu@163.com>
2018-05-05 15:43:21 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <compo></compo>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Compo from '@/components/comp/Compo'
+export default {
+  name: 'App',
+  components:{
+    Compo
+  }
+}
+</script>
+
+<style>
+#app {
+  padding-top:1rem;
+}
+</style>
---
+++ src/components/movie/Movie.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>movie</h1>
+</template>
+<script>
+  import store from '@/vuex/store'
+  export default({
+    store,
+    created () {
+      this.$store.commit('routerLinks',{
+        color:'rgb(33, 150, 243)',
+        title:'电影'
+      });
+    }
+  })
+</script>
+<style scoped>
+
+</style>
+
